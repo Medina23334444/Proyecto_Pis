f1c71428c2a56d3860224db36081ef5bec379f11
Medina23334444 <luis253363@gmail.com>
2023-07-13 21:36:45 -0500

    Carga Inicial Proyecto PIS

---
+++ src/competenciasdeportivas/CompetenciasDeportivas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package competenciasdeportivas;
+
+
+/**
+ *
+ * @author IdeaPad S340
+ */
+public class CompetenciasDeportivas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
